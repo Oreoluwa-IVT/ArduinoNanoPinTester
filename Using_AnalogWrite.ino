ed9a004217c59b00ed2db90e860aa1012cf42f77
Oreoluwa-IVT <75027292+Oreoluwa-IVT@users.noreply.github.com>
2021-07-25 17:44:41 +0100

    Update Using_AnalogWrite.ino

--- Using_AnalogWrite.ino
+++ Using_AnalogWrite.ino
@@ -1,19 +1,19 @@
-int a=A0;
-int b=9;
-void setup() {
+int a=A0; // assigning our Analog Pin "A0" as  "a "
+int b=9; //  assigning our Digital pin , Led pin "9" as  " b "
+void setup() { 
   // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(9,OUTPUT);
+Serial.begin(9600); // We instantiated Serial communication between the Arduino and the computer at 9600 bauds per second
+pinMode(b,OUTPUT); //  Pin 9/b is configured as an OUTPUT digital pin
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-int c=analogRead(a);
+int c=analogRead(a);  // we used the analogRead() function to read continuous varying values from our Analog pin "A0 / a "
 
-int okay=map(c,0,1023,0,255);
-Serial.println(okay);
+int okay=map(c,0,1023,0,255); // Because AnalogWrite functions only range from 0 to 255, we used the map function to get and equivalent of 0to 1023 into 0 to 255
+Serial.println(okay); // The value of okay is displayed on out serial monitor
 
-analogWrite(b,okay);
+analogWrite(b,okay);  // We used the analogWrite() function to generate varing digital output from 0 to 255
 
 
-}
+} // This closes the void loop() function
