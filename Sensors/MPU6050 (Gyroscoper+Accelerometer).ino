5e8aa325a873074096b8122c8283cd946aed1f62
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-10-10 15:43:23 +0100

    Motion tracking

---
+++ Sensors/MPU6050 (Gyroscoper+Accelerometer).ino
@@ -0,0 +1,19 @@
+/**Author: Oluwafemi Oreoluwa
+ * MPU6050 is an 8pin complete 6 axis motion tracking device.
+ * With Gyroscope , acceleromer and a DMP (Digital Motion Processor) on board,
+ * we can obtain data such as pitch , yaw and roll.
+ * In Summrary: The MPU6050 Module helps measure angular velocity , angle of tilt and 
+ * angle of inclination.
+ * 
+ *Pins: 
+ SDA: Serial Data pin
+ SCL: Serial Clock pin
+ INT: Interrup Digital Output pin
+ ADO: 12C 
+ GND: Ground Pin
+ VCC: Power(In) Supply Pin (+5v)
+ XDA: Auxiliary Serial Data Pin
+ XCL: Auxiliary Serial Clock Pin
+ * 
+**/
+
