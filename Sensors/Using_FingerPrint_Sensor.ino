ff75e03280b18478715cd77361c5b0a8445231ba
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-12-01 00:11:41 +0100

    Okay

--- Sensors/Using_FingerPrint_Sensor.ino
+++ Sensors/Using_FingerPrint_Sensor.ino
@@ -1,1 +1,27 @@
 //Biometric Authentication for DoorLock
+#include <Adafruit_Fingerprint.h>
+#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
+SoftwareSerial mySerial(2, 3);
+#else
+#define mySerial Serial1
+#endif
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  pinMode(13,OUTPUT);
+  Serial.println("\n\nFingePrint Detection");
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("FingerPrint Sensor Connected!");
+  } else {
+    Serial.println("Sensor Not Found !");
+    while (1) { delay(1); }
+  }
+
+  
